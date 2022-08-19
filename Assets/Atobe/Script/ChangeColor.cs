a5ba110896b89bab28f3d60c90936712d0df2c19
TakaiKentarou <mmpm664@yahoo.co.jp>
2022-08-19 15:55:19 +0900

    [update]InGame更新

--- Assets/Atobe/Script/ChangeColor.cs
+++ Assets/Atobe/Script/ChangeColor.cs
@@ -4,12 +4,6 @@ using UnityEngine;
 
 public class ChangeColor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        GetComponent<Renderer>().material.color = Color.red;
-    }
-
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("Player"))
